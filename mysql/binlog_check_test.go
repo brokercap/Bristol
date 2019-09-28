bd1933fabb98f15b30c44aaf5c01793a6d5ed06b
jc3wish <xiezuofu@xiezuofudeMacBook-Pro.local>
2019-09-28 22:21:30 +0800

    获取最近的正确位点,以QUEUE_EVENTY事件为准

--- mysql/binlog_check_test.go
+++ mysql/binlog_check_test.go
@@ -21,10 +21,11 @@ func TestGetNearestRightBinlog(t *testing.T)  {
 	filename:="mysql-bin.000068"
 	position:=uint32(484)
 
-	ReplicateDoDb := make(map[string]uint8,0)
-	ReplicateDoDb["test"] = 1
+	ReplicateDoDb := make(map[string]map[string]uint8,0)
 
-	newPosition := GetNearestRightBinlog(uri,filename,position,101,ReplicateDoDb)
+	ReplicateDoDb["bifrost_test"] = make(map[string]uint8,0)
+	ReplicateDoDb["bifrost_test"]["binlog_field_test"] = 1
+	newPosition := GetNearestRightBinlog(uri,filename,position,101,ReplicateDoDb,nil)
 
 	if newPosition == 0{
 		t.Fatal("error newPosition == 0")
